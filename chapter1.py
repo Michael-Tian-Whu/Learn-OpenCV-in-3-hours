1d2bbc2a1c8f14dc06f7c65497547ace4197349d
THC <1779733690@qq.com>
2022-10-25 16:28:55 +0800

    1

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,7 @@
 '''
 Author: WHURS-THC
 Date: 2022-10-21 20:37:07
-LastEditTime: 2022-10-22 16:37:21
+LastEditTime: 2022-10-25 10:43:52
 Description: 
 '''
 ####################### READ IMAGE ############################
@@ -14,27 +14,27 @@ Description:
 # print(type(a))
 
 # ######################## READ VIDEO #############################
-# import cv2
-# frameWidth = 640
-# frameHeight = 4806
-# cap = cv2.VideoCapture("Resources/test_ video.mp4")
-# while True:
-#     success, img = cap.read()
-#     img = cv2.resize(img, (frameWidth, frameHeight))
-#     cv2.imshow("Result", img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):# & ^ 与 或 异或运算 0xFF=1111 1111 取<255的部分,
-#         break
-# ######################### READ WEBCAM  ############################
 import cv2
 frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
+frameHeight = 4806
+cap = cv2.VideoCapture("Resources/test_video.mp4")
 while True:
     success, img = cap.read()
+    img = cv2.resize(img, (frameWidth, frameHeight))
     cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):# & ^ 与 或 异或运算 0xFF=1111 1111 取<255的部分,
         break
-ord('q')
+# ######################### READ WEBCAM  ############################
+# import cv2
+# frameWidth = 640
+# frameHeight = 480
+# cap = cv2.VideoCapture(0)
+# cap.set(3, frameWidth)
+# cap.set(4, frameHeight)
+# cap.set(10,150)
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Result", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# ord('q')
--- chapter5.py
+++ chapter5.py
@@ -1,7 +1,7 @@
 '''
 Author: WHURS-THC
 Date: 2022-10-21 20:37:07
-LastEditTime: 2022-10-24 20:52:38
+LastEditTime: 2022-10-24 21:51:06
 Description: 
 '''
 import cv2
@@ -10,10 +10,11 @@ import numpy as np
 img = cv2.imread("Resources/cards.jpg")
 
 width,height = 250,350
+dst=np.zeros((height,width),dtype=np.uint8)
 pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
 pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 matrix = cv2.getPerspectiveTransform(pts1,pts2)
-imgOutput = cv2.warpPerspective(img,matrix,(width,height))
+imgOutput = cv2.warpPerspective(img,matrix,(width+10,height+200),borderMode=cv2.BORDER_CONSTANT)
 
 cv2.imshow("Image",img)
 cv2.imshow("Output",imgOutput)
